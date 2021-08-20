89d28a958baf651ccfe21d5993c715f303883932
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-08-20 14:18:21 +0900

    rubocopの導入

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module Myapp
     config.generators.system_tests = nil
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
-        view_specs: false,
+        　　fixtures: false,
+        　view_specs: false,
         helper_specs: false,
         routing_specs: false
     end
