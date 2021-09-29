909d91e7e73ef0178d1bc7541f471ac391e529b5
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-09-29 17:32:19 +0900

    質問投稿機能の仮実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,28 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+    @question.save!
+    redirect_to questions_url, notice: "質問が作成されました"
   end
 
   def edit
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title,:information,:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  resource :questions
+  resources :questions
 end
