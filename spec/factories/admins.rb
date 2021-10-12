1dd57a373deef58b469b08345555aa14031558d8
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-13 01:46:52 +0900

    モデルのspecを実装[ci skip]

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :admin do
+    name { "管理者" }
+    sequence(:email) { |n| "admin_#{n}@example.com" }
+    password { "password" }
   end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前、メールアドレス、パスワードが設定されている管理者を登録できること" do
+    expect(FactoryBot.build(:admin)).to be_valid
+  end
+
+  it "名前が設定されていない管理者を登録できないこと" do
+    admin = FactoryBot.build(:admin, name: nil)
+    admin.valid?
+    expect(admin.errors[:name]).to include("を入力してください")
+  end
+
+  it "名前が20文字を超える管理者を登録できないこと" do
+    admin = FactoryBot.build(:admin, name: "a" * 21)
+    admin.valid?
+    expect(admin.errors[:name]).to include("は20文字以内で入力してください")
+  end
+
+  it "メールアドレスが設定されていない管理者を登録できないこと" do
+    admin = FactoryBot.build(:admin, email: nil)
+    admin.valid?
+    expect(admin.errors[:email]).to include("を入力してください")
+  end
+
+  it "同一のメールアドレスを設定して管理者を登録できないこと" do
+    FactoryBot.create(:admin, email: "admin@example.com")
+    admin = FactoryBot.build(:admin, email: "admin@example.com")
+    admin.valid?
+    expect(admin.errors[:email]).to include("はすでに存在します")
+  end
+
+  it "パスワードが設定されていない管理者を登録できないこと" do
+    admin = FactoryBot.build(:admin, password: nil)
+    admin.valid?
+    expect(admin.errors[:password]).to include("を入力してください")
+  end
+
+  it "パスワードが6文字に満たない管理者を登録できなこと" do
+    admin = FactoryBot.build(:admin, password: "a" * 5)
+    admin.valid?
+    expect(admin.errors[:password]).to include("は6文字以上で入力してください")
+  end
+
+  it "パスワードが128文字を超える管理者を登録できないこと" do
+    admin = FactoryBot.build(:admin, password: "a" * 129)
+    admin.valid?
+    expect(admin.errors[:password]).to include("は128文字以内で入力してください")
+  end
 end
