cae13ac5409cea03fb16432ff653a5b10110674e
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-13 23:30:29 +0900

    user用のカスタムビューとコントローラの生成[ci skip]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_keyword
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def search_results
+    @search_results = @q.result.order(created_at: :desc).page(params[:page]).per(5)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+
+  def set_keyword
+    @q = Question.ransack(params[:q])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions:       'admins/sessions',
+    passwords:      'admins/passwords',
+    registrations:  'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:       'users/sessions',
+    passwords:      'users/passwords',
+    registrations:  'users/registrations'
+  }
   root to: 'questions#index'
   resources :questions do
     post :confirm, action: :confirm_new, on: :new
