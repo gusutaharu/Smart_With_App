19157c961a6d896de68595bcfeaef9c12a78f00d
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-15 14:34:42 +0900

    ログインの有無でヘッダーの内容が変わる仕様を追加[ci skip]

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
   devise_for :admins, controllers: {
-    sessions:       'admins/sessions',
-    passwords:      'admins/passwords',
-    registrations:  'admins/registrations'
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations',
   }
   devise_for :users, controllers: {
-    sessions:       'users/sessions',
-    passwords:      'users/passwords',
-    registrations:  'users/registrations'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
   }
   resources :users, only: [:show]
   resources :questions do
