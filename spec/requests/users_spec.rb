5f49cb4599bb789ead14e45a5528ce68e57921ba
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-16 11:33:59 +0900

    redになっていた既存のspecがグリーンになる様に修正

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
   describe "GET /show" do
-    xit "returns http success" do
-      get "/users/show"
+    it "returns http success" do
+      get user_path(@user.id)
       expect(response).to have_http_status(:success)
     end
   end
