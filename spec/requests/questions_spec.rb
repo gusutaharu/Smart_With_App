b07850a7e0e3920f97aa072709ecab0a88cfb8b1
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-20 15:25:25 +0900

    refactor: 質問のrequestspecのリファクタリング

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,36 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Questions", type: :request do
-  let(:user) { FactoryBot.create(:user) }
-  let(:question) { FactoryBot.create(:question) }
+  let(:user_a) { create(:user) }
+  let(:other_user) { create(:user, name: "ohter_user") }
+  let!(:question_a) { create(:question, title: "user_aの質問", user_id: user_a.id) }
+  let(:question) { create(:question) }
+  let(:question_params) { attributes_for(:question) }
+  let(:invalid_question_params) { attributes_for(:question, title: "") }
 
   describe "GET /index" do
-    it "正常なレスポンスを返す" do
+    it "ステータスコードが200であること" do
       get root_path
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status 200
     end
   end
 
   describe "GET /show" do
-    it "正常なレスポンスを返す" do
+    it "ステータスコードが200であること" do
       get question_path(question.id)
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status 200
     end
   end
 
   describe "GET /new" do
+    subject { get new_question_path }
+
     context "ログインしている場合" do
-      it "正常なレスポンスを返すこと" do
-        sign_in user
-        get new_question_path
-        expect(response).to have_http_status(:success)
+      it "ステータスコードが200であること" do
+        sign_in user_a
+        is_expected.to eq 200
       end
     end
 
     context "ログインしていない場合" do
       it "ログインページにリダイレクすること" do
-        get new_question_path
-        expect(response).to redirect_to new_user_session_path
+        is_expected.to redirect_to new_user_session_path
       end
     end
   end
@@ -38,12 +42,12 @@ RSpec.describe "Questions", type: :request do
   describe "POST /confirm_new" do
     context "ログインしている場合" do
       before do
-        sign_in user
+        sign_in user_a
       end
 
       context "有効な属性の場合" do
         it "入力した質問が受け取れていること" do
-          question_params = FactoryBot.attributes_for(:question, title: "質問作成のフォームで入力した質問")
+          question_params = attributes_for(:question, title: "質問作成のフォームで入力した質問")
           post confirm_new_question_path, params: { question: question_params }
           expect(response.body).to include "質問作成のフォームで入力した質問"
         end
@@ -51,8 +55,7 @@ RSpec.describe "Questions", type: :request do
 
       context "無効な属性の場合" do
         it "有効な属性の入力を求めるエラーが表示されること" do
-          question_params = FactoryBot.attributes_for(:question, title: "")
-          post confirm_new_question_path, params: { question: question_params }
+          post confirm_new_question_path, params: { question: invalid_question_params }
           expect(response.body).to include "Titleを入力してください"
         end
       end
@@ -60,7 +63,6 @@ RSpec.describe "Questions", type: :request do
 
     context "ログインしていない場合" do
       it "ログインページにリダイレクすること" do
-        question_params = FactoryBot.attributes_for(:question)
         post confirm_new_question_path, params: { question: question_params }
         expect(response).to redirect_to new_user_session_path
       end
@@ -70,20 +72,18 @@ RSpec.describe "Questions", type: :request do
   describe "POST /create" do
     context "ログインしている場合" do
       before do
-        sign_in user
+        sign_in user_a
       end
 
       context "有効な属性の場合" do
         it "入力した質問が受け取れていること" do
-          question_params = FactoryBot.attributes_for(:question, title: "質問作成のフォームで入力した質問")
           expect { post questions_path, params: { question: question_params } }.to change(Question, :count).by(1)
         end
       end
 
       context "無効な属性の場合" do
         it "有効な属性の入力を求めるエラーが表示されること" do
-          question_params = FactoryBot.attributes_for(:question, title: "")
-          post questions_path, params: { question: question_params }
+          post questions_path, params: { question: invalid_question_params }
           expect(response.body).to include "Titleを入力してください"
         end
       end
@@ -91,7 +91,6 @@ RSpec.describe "Questions", type: :request do
 
     context "ログインしていない場合" do
       it "ログインページにリダイレクすること" do
-        question_params = FactoryBot.attributes_for(:question)
         post questions_path, params: { question: question_params }
         expect(response).to redirect_to new_user_session_path
       end
@@ -99,55 +98,47 @@ RSpec.describe "Questions", type: :request do
   end
 
   describe "GET /edit" do
-    let(:other_user) { FactoryBot.create(:user, name: "ohter_user") }
-    let(:my_question) { FactoryBot.create(:question, user_id: user.id) }
+    subject { get edit_question_path(question_a.id) }
 
     context "質問の投稿者がログインしている場合" do
-      it "正常なレスポンスを返す" do
-        sign_in user
-        get edit_question_path(my_question.id)
-        expect(response).to have_http_status(:success)
+      it "ステータスコードが200であること" do
+        sign_in user_a
+        is_expected.to eq 200
       end
     end
 
     context "質問の投稿主ではないユーザーがログインしている場合" do
       it "トップページにリダイレクトすること" do
         sign_in other_user
-        get edit_question_path(my_question.id)
-        expect(response).to redirect_to root_path
+        is_expected.to redirect_to root_path
       end
     end
 
     context "ログインしていない場合" do
       it "ログイン画面にリダイレクトすること" do
-        get edit_question_path(my_question.id)
-        expect(response).to redirect_to new_user_session_path
+        is_expected.to redirect_to new_user_session_path
       end
     end
   end
 
   describe "PATCH /update" do
-    let(:other_user) { FactoryBot.create(:user, name: "ohter_user") }
-    let(:question_a) { FactoryBot.create(:question, title: "更新前の質問", user_id: user.id) }
-
     context "質問の投稿者がログインしている場合" do
       before do
-        sign_in user
+        sign_in user_a
       end
 
       context "有効な属性の場合" do
         it "質問が更新されていること" do
-          question_params = FactoryBot.attributes_for(:question, title: "更新後の質問")
+          question_params = attributes_for(:question, title: "更新後の質問")
           expect { patch question_path question_a, params: { id: question_a.id, question: question_params } }.to change {
             question_a.reload.title
-          }.from("更新前の質問").to("更新後の質問")
+          }.from("user_aの質問").to("更新後の質問")
         end
       end
 
       context "無効な属性の場合" do
         it "有効な属性の入力を求めるエラーが表示されること" do
-          question_params = FactoryBot.attributes_for(:question, title: "")
-          patch question_path question_a, params: { id: question_a.id, question: question_params }
+          patch question_path question_a, params: { id: question_a.id, question: invalid_question_params }
           expect(response.body).to include "Titleを入力してください"
         end
       end
@@ -156,7 +147,6 @@ RSpec.describe "Questions", type: :request do
     context "質問の投稿者ではないユーザーがログインしている場合" do
       it "トップページにリダイレクトすること" do
         sign_in other_user
-        question_params = FactoryBot.attributes_for(:question)
         patch question_path question_a, params: { id: question_a.id, question: question_params }
         expect(response).to redirect_to root_path
       end
@@ -164,7 +154,6 @@ RSpec.describe "Questions", type: :request do
 
     context "ログインしていない場合" do
       it "ログインページにリダイレクすること" do
-        question_params = FactoryBot.attributes_for(:question)
         patch question_path question_a, params: { question: question_params }
         expect(response).to redirect_to new_user_session_path
       end
@@ -172,28 +161,25 @@ RSpec.describe "Questions", type: :request do
   end
 
   describe "DELETE /destroy" do
-    let(:other_user) { FactoryBot.create(:user, name: "ohter_user") }
-    let!(:my_question) { FactoryBot.create(:question, user_id: user.id) }
+    subject { delete question_url question_a }
 
     context "質問の投稿者がログインしている場合" do
       it "質問を削除できること" do
-        sign_in user
-        expect { delete question_url my_question }.to change(Question, :count).by(-1)
+        sign_in user_a
+        expect { subject }.to change(Question, :count).by(-1)
       end
     end
 
     context "質問の投稿者ではないユーザーがログインしている場合" do
       it "トップページにリダイレクトすること" do
         sign_in other_user
-        delete question_path my_question
-        expect(response).to redirect_to root_path
+        is_expected.to redirect_to root_path
       end
     end
 
     context "ログインしていない場合" do
       it "ログインページにリダイレクすること" do
-        delete question_path my_question
-        expect(response).to redirect_to new_user_session_path
+        is_expected.to redirect_to new_user_session_path
       end
     end
   end
