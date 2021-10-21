11911698e864b475e5b72229cfb36e103bec0502
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-22 04:28:30 +0900

    add: users_sessions_spec.rb 内のテストを追加

--- spec/requests/users_sessions_spec.rb
+++ spec/requests/users_sessions_spec.rb
@@ -1,36 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSessions", type: :request do
+  let!(:user_a) { create(:user, name: "user_a", email: "user_a@email.com", password: "password") }
+  let(:user_params) { attributes_for(:user, email: "user_a@email.com", password: "password") }
 
   describe "GET /resource/sign_in" do
-    xcontext "ログインをしている場合" do
-      it "" do
+    subject { get new_user_session_path }
+
+    context "ログインをしている場合" do
+      it "トップページにリダイレクトされること" do
+        sign_in user_a
+        is_expected.to redirect_to root_path
       end
     end
-    xcontext "ログインをしていない場合" do
-      it "" do
+
+    context "ログインをしていない場合" do
+      it "リクエストが成功すること" do
+        is_expected.to eq 200
       end
     end
   end
 
   describe "POST /resource/sign_in" do
-    xcontext "ログインをしている場合" do
-      it "" do
+    subject { post new_user_session_path, params: { user: user_params } }
+
+    context "ログインをしている場合" do
+      it "トップページにリダイレクトされること" do
+        sign_in user_a
+        is_expected.to redirect_to root_path
       end
     end
-    xcontext "ログインをしていない場合" do
-      it "" do
+
+    context "ログインをしていない場合" do
+      it "リクエストが成功すること" do
+        is_expected.to eq 302
       end
     end
   end
 
   describe "DELETE /resource/sign_out" do
-    xcontext "ログインをしている場合" do
-      it "" do
+    context "ログインをしている場合" do
+      it "トップページにリダイレクトされること" do
+        sign_in user_a
+        delete destroy_user_session_path
+        expect(response).to redirect_to root_path
       end
     end
-    xcontext "ログインをしていない場合" do
-      it "" do
+
+    context "ログインをしていない場合" do
+      it "トップページにリダイレクトされないこと" do
+        delete destroy_user_session_path user_a
+        expect(response).not_to redirect_to root_path
       end
     end
   end
