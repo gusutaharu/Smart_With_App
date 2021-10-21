eac783846b7a612d1b8d76c437265c0ab7e4112b
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-21 13:25:20 +0900

    add: PATCH /resourceのspecを追加

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Questions", type: :request do
   let!(:user_a) { create(:user, name: "user_a") }
   let(:other_user) { create(:user, name: "other_user") }
   let(:user_params) { attributes_for(:user, name: "new_name") }
-  let(:invaild_user_params) { attributes_for(:user, name: "") }
+  let(:invalid_user_params) { attributes_for(:user, name: "") }
 
   describe "GET /resource/sign_up" do
     context "ログインをしている場合" do
@@ -41,10 +41,10 @@ RSpec.describe "Questions", type: :request do
 
       context "無効な属性の場合" do
         it "ユーザーが登録されないこと" do
-          expect { post user_registration_path, params: { user: invaild_user_params } }.not_to change(User, :count)
+          expect { post user_registration_path, params: { user: invalid_user_params } }.not_to change(User, :count)
         end
         it "エラーが表示されること" do
-          post user_registration_path, params: { user: invaild_user_params }
+          post user_registration_path, params: { user: invalid_user_params }
           expect(response.body).to include "名前を入力してください"
         end
       end
@@ -91,14 +91,18 @@ RSpec.describe "Questions", type: :request do
         end
       end
 
-      xcontext "無効な属性の場合" do
-        it "" do
+      context "無効な属性の場合" do
+        it "エラーが表示されること" do
+          patch user_registration_path, params: { user: invalid_user_params }
+          expect(response.body).to include "名前を入力してください"
         end
       end
     end
 
-    xcontext "ログインしていない場合" do
-      it "" do
+    context "ログインしていない場合" do
+      it "リダイレクトされてること" do
+        patch user_registration_path, params: { user: invalid_user_params }
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
