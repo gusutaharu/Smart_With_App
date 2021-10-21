0cab03ab613ee6f54af18030a8c2ba77f552f9dd
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-10-21 17:28:52 +0900

    add: requests/users_sessions_spec.rbを追加

---
+++ spec/requests/users_sessions_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSessions", type: :request do
+
+  describe "GET /resource/sign_in" do
+    xcontext "ログインをしている場合" do
+      it "" do
+      end
+    end
+    xcontext "ログインをしていない場合" do
+      it "" do
+      end
+    end
+  end
+
+  describe "POST /resource/sign_in" do
+    xcontext "ログインをしている場合" do
+      it "" do
+      end
+    end
+    xcontext "ログインをしていない場合" do
+      it "" do
+      end
+    end
+  end
+
+  describe "DELETE /resource/sign_out" do
+    xcontext "ログインをしている場合" do
+      it "" do
+      end
+    end
+    xcontext "ログインをしていない場合" do
+      it "" do
+      end
+    end
+  end
+end
