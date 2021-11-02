bc3c8c28e85017e9ab9664522029fb3584d67aed
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-11-03 03:43:25 +0900

    add: 質問作成時に投稿する画像のプレビュー表示を実装 #5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "../styles/application.scss";
+import "../src/component.js";
 
 require('bootstrap');
 
