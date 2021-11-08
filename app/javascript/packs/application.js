92c964f0e22027c0a9c920d24c9c70fcd4adb9c6
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-11-08 16:17:30 +0900

    feat: ユーザー画像設定時にもプレビュー表示機能実装 #5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,9 @@ import "../styles/application.scss";
 import "../src/preview.js";
 import "../src/form_data.js";
 import "../src/img_zoom.js";
+import "../src/icon_preview.js";
 
-require('bootstrap');
+require("bootstrap");
 
 Rails.start()
 Turbolinks.start()
