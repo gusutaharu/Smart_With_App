b13bb15bc973ca4b9f7a732e0427b1047ae45963
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-11-08 21:22:03 +0900

    feat: 質問に添付する画像枚数を制限 #5

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,17 @@
 class Question < ApplicationRecord
+  IMAGES_NUMBER_LIMIT = 4
   belongs_to :user
   mount_uploaders :question_images, QuestionImagesUploader
   validates :title, presence: true, length: { maximum: 20 }
   validates :information, presence: true
   validates :content, presence: true
   validates :user_id, presence: true
+  validate :validate_number_of_images
+
+  private
+
+  def validate_number_of_images
+    return if question_images.length <= IMAGES_NUMBER_LIMIT
+    errors.add(:question, "に添付できる画像は#{IMAGES_NUMBER_LIMIT}件までです")
+  end
 end
