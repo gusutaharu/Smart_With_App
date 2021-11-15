972c4e62f2d6f7448003e8797a9869da707e7f5e
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-11-16 00:07:49 +0900

    refactor: rubocopの指摘箇所を修正

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,7 +2,7 @@ class InterestsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @interest = Interest.new(user_id: current_user.id,question_id: params[:question_id])
+    @interest = Interest.new(user_id: current_user.id, question_id: params[:question_id])
     @interest.save
     @question = @interest.question
   end
