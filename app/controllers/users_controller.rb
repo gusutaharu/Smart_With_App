bf22609761729de2d88899ef9675e126fa81b359
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-11-24 15:55:54 +0900

    feat: 回答数や回答した質問に回答へのコメントが含まれないように修正 #10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @questions = @user.questions
     interests = Interest.where(user_id: @user.id).order(created_at: :desc).pluck(:question_id)
     @interesting_questions = Question.find(interests)
-    answers = Answer.where(user_id: @user.id).order(created_at: :desc).pluck(:question_id)
+    answers = Answer.where(user_id: @user.id, parent_id: nil).order(created_at: :desc).pluck(:question_id)
     @answered_questions = Question.find(answers)
   end
 end
