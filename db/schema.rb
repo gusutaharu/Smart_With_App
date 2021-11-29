e3bbe0ac4399b11b09599ddb5e5aabb83f85e911
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2021-11-29 22:30:43 +0900

    add: カテゴリーモデルの用意 #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_133420) do
+ActiveRecord::Schema.define(version: 2021_11_29_113316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_11_22_133420) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "parent_id"
     t.index ["parent_id"], name: "index_answers_on_parent_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "interests", force: :cascade do |t|
